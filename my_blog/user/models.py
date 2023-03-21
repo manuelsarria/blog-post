ab3939820759c8a3855156fc9d9d57d14c56af3e
Manuel Sarrria <victor.sarria@utp.ac.pa>
2023-03-21 18:18:17 -0500

    add git ignore

--- my_blog/user/models.py
+++ my_blog/user/models.py
@@ -3,4 +3,8 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+  email = models.EmailField(unique=True)
   web_site = models.CharField(max_length=255, blank=True)
+  
+  USERNAME_FIELD = 'email'
+  REQUIRED_FIELDS = []
