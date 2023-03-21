2e5e9ba0a41fe6d0ee773b14f0ef6da33ba0371b
Manuel Sarrria <victor.sarria@utp.ac.pa>
2023-03-21 17:05:54 -0500

    add documentation with swagger and redoc

--- my_blog/my_blog/settings.py
+++ my_blog/my_blog/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'posts',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
@@ -118,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = './static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- my_blog/my_blog/urls.py
+++ my_blog/my_blog/urls.py
@@ -15,10 +15,27 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from posts.api.router import router_post
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Documentacion API BLOG",
+      default_version='v1',
+      description="Documentacion de api blog",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('api/posts/', PostViewSet.as_view()),
-    path('api/', include(router_post.urls))
+    path('api/', include(router_post.urls)),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+ 
