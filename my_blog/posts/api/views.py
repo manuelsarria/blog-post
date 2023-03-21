1c3afb9d8ba428bcf210672cc9a41de19ab66fef
Manuel Sarrria <victor.sarria@utp.ac.pa>
2023-03-21 16:02:13 -0500

    add viewset modelviewset

--- my_blog/posts/api/views.py
+++ my_blog/posts/api/views.py
@@ -1,17 +1,50 @@
 from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework.viewsets import ViewSet, ModelViewSet
 from rest_framework.response import Response
 from posts.models import Post
+from posts.api.serializer import PostSerializer
 
-class PostApiView(APIView):
-  def get(self, request):
-    # posts = Post.objects.all()
-    posts = [post.title for post in Post.objects.all()]
-    return Response(status=status.HTTP_200_OK, data=posts)
-  
-  def post(self, request):
-    Post.objects.create(title=request.POST['title'], description=request.POST['description'],
-                        order=request.POST['order'])
-    return self.get(request)
+class PostModelViewSet(ModelViewSet):
+  serializer_class = PostSerializer
+  queryset = Post.objects.all()
   
 
+# class PostViewSet(ViewSet):
+#   def list(self, request):
+#     # posts = Post.objects.all()
+#     # posts = [post.title for post in Post.objects.all()]
+#     serializer = PostSerializer(Post.objects.all(), many=True)
+#     return Response(status=status.HTTP_200_OK, data=serializer.data)
+  
+#   def retrieve(self, request, pk: int):
+#     post = PostSerializer(Post.objects.get(pk=pk))
+#     return Response(status=status.HTTP_200_OK, data=post.data)
+  
+#   def create(self, request):
+#     # Post.objects.create(title=request.POST['title'], description=request.POST['description'],
+#     #                     order=request.POST['order'])
+#     # return self.get(request)
+#     serializer = PostSerializer(data=request.POST)
+#     serializer.is_valid(raise_exception=True)
+#     serializer.save()
+#     return Response(status=status.HTTP_200_OK,  data=serializer.data)
+
+# class PostApiView(APIView):
+#   def get(self, request):
+#     # posts = Post.objects.all()
+#     # posts = [post.title for post in Post.objects.all()]
+#     serializer = PostSerializer(Post.objects.all(), many=True)
+#     return Response(status=status.HTTP_200_OK, data=serializer.data)
+  
+#   def post(self, request):
+#     # Post.objects.create(title=request.POST['title'], description=request.POST['description'],
+#     #                     order=request.POST['order'])
+#     # return self.get(request)
+#     serializer = PostSerializer(data=request.POST)
+#     serializer.is_valid(raise_exception=True)
+#     serializer.save()
+#     return Response(status=status.HTTP_200_OK,  data=serializer.data)
+  
+  
+  
